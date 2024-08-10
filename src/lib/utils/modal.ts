8fbfacdc9e615a02f0e74dd1de185a25fab17046
fyodorio <alinetofyodor@gmail.com>
2024-08-11 01:07:00 +0500

    fix: make modal work with document to prevent build failure

--- src/lib/utils/modal.ts
+++ src/lib/utils/modal.ts
@@ -15,61 +15,73 @@ let visibleModal = null;
 
 // Toggle modal
 export const toggleModal = (event) => {
-  event.preventDefault();
-  const modal = document.getElementById(event.currentTarget.dataset.target) as HTMLDialogElement;
-  if (!modal) return;
-  modal && (modal.open ? closeModal(modal) : openModal(modal));
+  if (typeof document !== 'undefined') {
+    event.preventDefault();
+    const modal = document.getElementById(event.currentTarget.dataset.target) as HTMLDialogElement;
+    if (!modal) return;
+    modal && (modal.open ? closeModal(modal) : openModal(modal));
+  }
 };
 
 // Open modal
 const openModal = (modal) => {
-  const { documentElement: html } = document;
-  const scrollbarWidth = getScrollbarWidth();
-  if (scrollbarWidth) {
-    html.style.setProperty(scrollbarWidthCssVar, `${scrollbarWidth}px`);
+  if (typeof document !== 'undefined') {
+    const { documentElement: html } = document;
+    const scrollbarWidth = getScrollbarWidth();
+    if (scrollbarWidth) {
+      html.style.setProperty(scrollbarWidthCssVar, `${scrollbarWidth}px`);
+    }
+    html.classList.add(isOpenClass, openingClass);
+    setTimeout(() => {
+      visibleModal = modal;
+      html.classList.remove(openingClass);
+    }, animationDuration);
+    modal.showModal();
   }
-  html.classList.add(isOpenClass, openingClass);
-  setTimeout(() => {
-    visibleModal = modal;
-    html.classList.remove(openingClass);
-  }, animationDuration);
-  modal.showModal();
 };
 
 // Close modal
 const closeModal = (modal) => {
-  visibleModal = null;
-  const { documentElement: html } = document;
-  html.classList.add(closingClass);
-  setTimeout(() => {
-    html.classList.remove(closingClass, isOpenClass);
-    html.style.removeProperty(scrollbarWidthCssVar);
-    modal.close();
-  }, animationDuration);
+  if (typeof document !== 'undefined') {
+    visibleModal = null;
+    const { documentElement: html } = document;
+    html.classList.add(closingClass);
+    setTimeout(() => {
+      html.classList.remove(closingClass, isOpenClass);
+      html.style.removeProperty(scrollbarWidthCssVar);
+      modal.close();
+    }, animationDuration);
+  }
 };
 
 // Close with a click outside
-document.addEventListener('click', (event) => {
-  if (visibleModal === null) return;
-  const modalContent = (visibleModal as HTMLDialogElement).querySelector('article');
-  const isClickInside = modalContent!.contains((event as MouseEvent).target as Node);
-  !isClickInside && closeModal(visibleModal);
-});
+if (typeof document !== 'undefined') {
+  document.addEventListener('click', (event) => {
+    if (visibleModal === null) return;
+    const modalContent = (visibleModal as HTMLDialogElement).querySelector('article');
+    const isClickInside = modalContent!.contains((event as MouseEvent).target as Node);
+    !isClickInside && closeModal(visibleModal);
+  });
 
-// Close with Esc key
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape' && visibleModal) {
-    closeModal(visibleModal);
-  }
-});
+  // Close with Esc key
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && visibleModal) {
+      closeModal(visibleModal);
+    }
+  });
+}
 
 // Get scrollbar width
 const getScrollbarWidth = () => {
-  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-  return scrollbarWidth;
+  if (typeof document !== 'undefined') {
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    return scrollbarWidth;
+  }
 };
 
 // Is scrollbar visible
 const isScrollbarVisible = () => {
-  return document.body.scrollHeight > screen.height;
+  if (typeof document !== 'undefined') {
+    return document.body.scrollHeight > screen.height;
+  }
 };
