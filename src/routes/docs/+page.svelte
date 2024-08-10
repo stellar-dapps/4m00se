1a37e5b210f9d728915e6b97e6e0a002fbab5a68
fyodorio <alinetofyodor@gmail.com>
2024-08-10 17:51:46 +0500

    feat: add markdown

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,7 +1,12 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import MarkdownIt from "markdown-it";
+  import my_markdown from "$lib/content/docs/getting-started.md?raw";
+
+  const md = new MarkdownIt();
+</script>
 
 <svelte:head>
   <title>4m00se — Docs</title>
 </svelte:head>
 
-<h1>Welcome to <strong>4m00se</strong> Documentation</h1>
+{@html md.render(my_markdown)}
